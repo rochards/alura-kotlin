fecb464ac6d459539ca00bd9a9f7e472a393b722
Rodrigo Rocha <rodrigorochasantos@outlook.com>
2021-05-04 14:59:04 -0300

    Add classe Funcionario

---
+++ bytebank/src/main/kotlin/br/com/alura/domain/Funcionario.kt
@@ -0,0 +1,11 @@
+package br.com.alura.domain
+
+class Funcionario(
+    val nome: String,
+    val cpf: String,
+    var salario: Double
+) {
+    fun bonificacao(): Double {
+        return salario * 0.1
+    }
+}
--- bytebank/src/main/kotlin/br/com/alura/main.kt
+++ bytebank/src/main/kotlin/br/com/alura/main.kt
@@ -1,5 +1,11 @@
 package br.com.alura
 
+import br.com.alura.domain.Funcionario
+
 fun main(args: Array<String>) {
 
+    val f1 = Funcionario("Luiz", "123.654.321.31", 1000.0)
+    println("nome: ${f1.nome}")
+    println("cpf: ${f1.cpf}")
+    println("salario: ${f1.salario}")
 }
