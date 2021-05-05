142b3cfdde926e33b616d2f896b8267c134c5ef6
Rodrigo Rocha <rodrigorochasantos@outlook.com>
2021-05-05 10:44:27 -0300

    Refatorações

--- bytebank/src/main/kotlin/br/com/alura/testaFuncionarios.kt
+++ bytebank/src/main/kotlin/br/com/alura/testaFuncionarios.kt
@@ -3,12 +3,9 @@ package br.com.alura
 import br.com.alura.domain.Analista
 import br.com.alura.domain.Diretor
 import br.com.alura.domain.Gerente
-import br.com.alura.domain.SistemaInterno
 import br.com.alura.utils.TotalDeBonificacao
 
 fun testaFuncionarios() {
-
-    val sistemaInterno = SistemaInterno()
 
     val f1 = Analista("Luiz", "123.654.321.31", 1000.0)
     println("nome: ${f1.nome}")
@@ -22,7 +19,6 @@ fun testaFuncionarios() {
     println("cpf: ${f2.cpf}")
     println("salario: ${f2.salario}")
     println("bonificação: ${f2.bonificacao()}")
-    sistemaInterno.login(f2, "senha")
 
     val f3 = Diretor("Lima", "247.874.347-36", 19500.57, "secret2", 1500.0)
     println()
@@ -30,7 +26,6 @@ fun testaFuncionarios() {
     println("cpf: ${f3.cpf}")
     println("salario: ${f3.salario}")
     println("bonificação: ${f3.bonificacao()}")
-    sistemaInterno.login(f3, "senha")
 
     val totalBonificacao = TotalDeBonificacao()
     totalBonificacao.registraBonificacao(f1)
